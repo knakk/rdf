ec3809c02d1b07bdb4a051af670e0741464ef71f
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2015-04-08 22:19:16 +0900

    Update docstrings

--- decoder.go
+++ decoder.go
@@ -6,29 +6,48 @@ import (
 	"runtime"
 )
 
-// A ParseOption allows to customize the behaviour of decoders.
+// A ParseOption allows to customize the behaviour of a decoder.
 type ParseOption int
 
-// Options which can configure the decoders.
+// Options which can configure a decoder.
 const (
-	Base ParseOption = iota // Base IRI to resolve relative IRIs against.
+	// Base IRI to resolve relative IRIs against (for formats that support
+	// relative IRIs: Turtle, RDF/XML, TriG, JSON-LD)
+	Base ParseOption = iota
+
+	// Strict mode determines how the decoder responds to errors.
+	// When true (the default), it will fail on any malformed input. When
+	// false, it will try to continue parsing, discarding only the malformed
+	// parts.
+	// Strict
+
+	// ErrOut
 )
 
 // TripleDecoder parses RDF documents (serializations of an RDF graph).
 //
 // For streaming parsing, use the Decode() method to decode a single Triple
 // at a time. Or, if you want to read the whole document in one go, use DecodeAll().
+//
+// The decoder can be instructed with numerous options. Note that not all options
+// are supported by all formats. Consult the following table:
+//
+//  Option      Description        Value      (default)       Format support
+//  ------------------------------------------------------------------------------
+//  Base        Base IRI           IRI        (empty IRI)     Turtle, RDF/XML
+//  Strict      Strict mode        true/false (true)          TODO
+//  ErrOut      Error output       io.Writer  (nil)           TODO
 type TripleDecoder interface {
-	// Decode should parse a RDF document and return the next valid triple.
-	// It should return io.EOF when the whole document is parsed.
+	// Decode parses a RDF document and return the next valid triple.
+	// It returns io.EOF when the whole document is parsed.
 	Decode() (Triple, error)
 
-	// DecodeAll should parse the entire RDF document and return all valid
+	// DecodeAll parses the entire RDF document and return all valid
 	// triples, or an error.
 	DecodeAll() ([]Triple, error)
 
 	// SetOption sets a parsing option to the given value. Not all options
-	// are supported by all decoders.
+	// are supported by all serialization formats.
 	SetOption(ParseOption, interface{}) error
 }
 
